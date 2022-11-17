503298cc61ce26e319511092193e081bee1d0dd3
Riaz <imriaz.cu@gmail.com>
2022-11-17 11:48:10 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+// $(document).ready(function(){
+//     $('header > .toggle-button').click(function(){
+//         $('#navbar').slideToggle();
+//     })
+// })
+
+/* Sidebar */
+
+const sidebar = document.querySelector('#sidebar');
+const sidebarToggler = document.querySelector('.sidebar_toggler');
+
+
+// Toggling the Sidebar
+sidebarToggler.addEventListener('click', () => {
+    sidebar.classList.toggle('show');
+});
+
+
+// Closing the Sidebar on clicking Outside and on the Sidebar-Links
+window.addEventListener('click', (e) => {
+    if (e.target.id !== 'sidebar' && e.target.className !== 'sidebar_toggler') {
+        sidebar.classList.remove('show');
+    }
+});
